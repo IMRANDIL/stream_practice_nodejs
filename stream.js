cdc94807b1bde1ba2a55c8f2bf316697300bc2f8
IMRANDIL <aliimranadil2@gmail.com>
2023-09-10 21:40:25 +0530

    added gitignore

--- stream.js
+++ stream.js
@@ -1,14 +1,29 @@
-const fs = require('node:fs/promises');
+// const fs = require('node:fs/promises');
 
 
 
-(async()=>{   
-const fileHandle = await fs.open('test.txt', 'w');
-console.time('process')
-for(let i=0; i<1000000; i++){
-    await fileHandle.write(`${i} `)
+// (async()=>{   
+// const fileHandle = await fs.open('test.txt', 'w');
+// console.time('process')
+// for(let i=0; i<1000000; i++){
+//     await fileHandle.write(`${i} `)
+// }
+// console.timeEnd('process')
+
+// })()
+
+
+const fs = require('fs');
+
+
+(async()=>{
+
+fs.open('test.txt', 'w', (err, fd)=>{
+    console.time('process')
+for(let i = 0; i<1000000; i++){
+    fs.writeSync(fd, `${i} `)
 }
 console.timeEnd('process')
+})
 
 })()
-
