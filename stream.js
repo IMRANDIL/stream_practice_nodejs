dd4066e9f11ba5352ffdb79c0b6affde390a508c
IMRANDIL <aliimranadil2@gmail.com>
2023-09-10 22:49:08 +0530

    added some more stream methods

--- stream.js
+++ stream.js
@@ -34,38 +34,38 @@
 //streams gpt way>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 
-const fs = require('node:fs/promises');
+// const fs = require('node:fs/promises');
 
-let i = 0;
-const totalRecords = 1000000;
-const batchSize = 10000; // Adjust this as needed
+// let i = 0;
+// const totalRecords = 1000000;
+// const batchSize = 10000; // Adjust this as needed
 
-const writeStream = async (stream) => {
-  while (i < totalRecords) {
-    for (let j = 0; j < batchSize && i < totalRecords; j++) {
-      const buff = Buffer.from(` ${i} `, 'utf-8');
-      i++;
-      if (!stream.write(buff)) {
-        // If the stream is not writable, pause and wait for 'drain' event
-        await new Promise((resolve) => stream.once('drain', resolve));
-      }
-    }
-  }
-};
+// const writeStream = async (stream) => {
+//   while (i < totalRecords) {
+//     for (let j = 0; j < batchSize && i < totalRecords; j++) {
+//       const buff = Buffer.from(` ${i} `, 'utf-8');
+//       i++;
+//       if (!stream.write(buff)) {
+//         // If the stream is not writable, pause and wait for 'drain' event
+//         await new Promise((resolve) => stream.once('drain', resolve));
+//       }
+//     }
+//   }
+// };
 
-(async () => {
-  console.time('process');
-  const fileHandle = await fs.open('test.txt', 'w');
-  const stream = fileHandle.createWriteStream();
+// (async () => {
+//   console.time('process');
+//   const fileHandle = await fs.open('test.txt', 'w');
+//   const stream = fileHandle.createWriteStream();
 
-  stream.on('finish', () => {
-    console.timeEnd('process');
-    fileHandle.close()
-  });
+//   stream.on('finish', () => {
+//     console.timeEnd('process');
+//     fileHandle.close()
+//   });
 
-  await writeStream(stream);
-  stream.end(); // Close the stream when done writing
-})();
+//   await writeStream(stream);
+//   stream.end(); // Close the stream when done writing
+// })();
 
 
 
@@ -73,37 +73,41 @@ const writeStream = async (stream) => {
 
 //streams starts now>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-// const fs = require('node:fs/promises');
+const fs = require('node:fs/promises');
 
-// let i =0;
+(async()=>{   
+    console.time('process')
+const fileHandle = await fs.open('test.txt', 'w');
 
-// const writeStream = ()=>{
-//     while( i<1000000){
-//         const buff = Buffer.from(` ${i} `, 'utf-8');
+const stream = fileHandle.createWriteStream();
+let i =0;
 
-//         i++;
-//         if(!stream.write(buff)) break;
-//         stream.write(buff)
+const writeStream = ()=>{
+    while( i<1000000){
+        const buff = Buffer.from(` ${i} `, 'utf-8');
+      
+        if (i === 999999){
+           return stream.end(buff)
+        }
+        if(!stream.write(buff)) break;
+       
+
+        i++;
+       
         
-//     }
-// }
+    }
+}
+
+writeStream();
+
+stream.on('drain', ()=>{
+    writeStream()
+})
+
+stream.on('finish', ()=>{
+    console.timeEnd('process')
+    fileHandle.close()
+})
 
 
-
-// (async()=>{   
-//     console.time('process')
-// const fileHandle = await fs.open('test.txt', 'w');
-
-// const stream = fileHandle.createWriteStream();
-
-
-// stream.on('drain', ()=>{
-//     writeStream()
-// })
-
-// stream.on('finish', ()=>{
-//     console.timeEnd('process')
-// })
-
-
-// })()
+})()
